debe8a96546410cd6041d97858e68d73272396be
Timothee Brindejonc <timotdu54@gmail.com>
2023-05-07 12:16:58 +0200

    feat: add type for answers when creating project

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,11 @@ export type Question = {
 }
 
 export type QuestionChoice = Array<Question>;
+
+export type CreateProjectAnswers = {
+    webTech: string;
+    webFramework: string;
+    tailwind: boolean;
+    eslint: boolean;
+    name: string;
+}
