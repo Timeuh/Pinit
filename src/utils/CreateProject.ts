7babc345d66d435977ed650df56b165e06262ee2
Timothee Brindejonc <timotdu54@gmail.com>
2023-05-07 12:17:23 +0200

    feat: add CreateProject script to create new directory and npm init

---
+++ src/utils/CreateProject.ts
@@ -0,0 +1,26 @@
+import {CreateProjectAnswers} from "../types/types";
+import fs from "fs";
+import chalk from "chalk";
+import path from "path";
+import {join} from "path";
+import {spawn} from "child_process";
+
+export const createProject = (answers: CreateProjectAnswers) => {
+    fs.mkdir(answers.name, (err) => {
+        if (err) {
+            console.log(chalk.red('Error while creating project folder !'));
+            return;
+        }
+
+        const directoryDir = path.resolve(process.cwd(), answers.name);
+        process.chdir(directoryDir);
+
+        const appData = process.env.APPDATA ? process.env.APPDATA : '';
+        const npmPath = join(appData, 'npm', 'npm.cmd');
+
+        const npmInit = spawn(npmPath, ['init', '-y'], {stdio: 'ignore'});
+        npmInit.on('error', (err) => {
+            console.log(chalk.red('Error while initializing npm !'));
+        });
+    });
+}
