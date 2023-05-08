a88f913eacb7582cab0c682b4c55a4cbeed440c2
Timeuh <timotdu54@gmail.com>
2023-05-08 22:11:50 +0200

    feat: ask for framework first, then ask for other params

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,7 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import {createProject} from "./utils/CreateProject";
 import {initVite} from "./utils/UseVite";
-import {techs, templates, useTemplate, useVite, viteParams} from "./stores/appConsts";
+import {projectParams, templates, useFramework, useTemplate, useVite, useWebTech, viteParams} from "./stores/appConsts";
 
 // main function of the CLI
 export function cli() {
@@ -33,9 +33,22 @@ export function cli() {
                                 });
                         } else {
                             // if no, ask him which techs he wants to use
-                            inquirer.prompt(techs)
+                            inquirer.prompt(useFramework)
                                 .then(answers => {
-                                    createProject(answers);
+                                    if (answers.framework === 'None') {
+                                        inquirer.prompt(useWebTech)
+                                            .then(answer => {
+                                                inquirer.prompt(projectParams)
+                                                    .then(answers => {
+                                                        createProject(answer.webTech, answers);
+                                                    });
+                                            });
+                                    } else {
+                                        inquirer.prompt(projectParams)
+                                            .then(answers => {
+                                                createProject('none', answers);
+                                            });
+                                    }
                                 });
                         }
                     });
