6a9f6d6f4ad7281887387ec93b41373eb07edfe5
Timeuh <timotdu54@gmail.com>
2023-05-08 21:15:07 +0200

    refactor: use only npm init vite and not npm install

--- src/utils/UseVite.ts
+++ src/utils/UseVite.ts
@@ -4,18 +4,9 @@ import {npmPath} from "../stores/appConsts";
 
 // init Vite js if the user chose it
 export const initVite = () => {
-    // install vite
-    const viteInstall = spawn(npmPath, ['install', 'vite', '--save-dev'], {stdio: 'ignore'});
-    viteInstall.on('error', () => {
-        console.log(chalk.red('Error while installing Vite !'));
-    });
-
-    // when vite is installed, init it
-    viteInstall.on('close', () => {
-        // init vite
-        const viteInit = spawn(npmPath, ['init', 'vite'], {stdio: 'inherit'});
-        viteInit.on('error', () => {
-            console.log(chalk.red('Error while initializing Vite !'));
-        });
+    // init vite
+    const viteInit = spawn(npmPath, ['init', 'vite'], {stdio: 'inherit'});
+    viteInit.on('error', () => {
+        console.log(chalk.red('Error while initializing Vite !'));
     });
 }
