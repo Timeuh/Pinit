138577147f0826fb830a3162bc4d0110c7c51572
Timeuh <timotdu54@gmail.com>
2023-05-08 22:50:36 +0200

    refactor-doc: give createProject right params, rename inquirer different answers, add comments

--- src/cli.ts
+++ src/cli.ts
@@ -10,43 +10,50 @@ export function cli() {
 
     // ask the user if he wants to use a template
     inquirer.prompt(useTemplate)
-        .then(answers => {
-            if (answers.useTemplate) {
+        .then(template => {
+            if (template.useTemplate) {
                 // if yes, ask him which one
                 inquirer.prompt(templates)
                     .then(answers => {
                         console.log(answers);
                     });
             } else {
+                // if no, ask if he wants to use vite js
                 inquirer.prompt(useVite)
-                    .then(answers => {
-                        if (answers.useVite) {
+                    .then(vite => {
+                        // if yes, ask him if he wants eslint and the project name
+                        if (vite.useVite) {
                             inquirer.prompt(viteParams)
-                                .then(answers => {
-                                    if (answers.useEslint){
+                                .then(viteParams => {
+                                    if (viteParams.useEslint){
                                         // use vite with eslint
-                                        initVite(true, answers.name);
+                                        initVite(true, viteParams.name);
                                     } else {
                                         // use vite without eslint
-                                        initVite(false, answers.name);
+                                        initVite(false, viteParams.name);
                                     }
                                 });
                         } else {
-                            // if no, ask him which techs he wants to use
+                            // if no, ask him which framework he wants to use
                             inquirer.prompt(useFramework)
-                                .then(answers => {
-                                    if (answers.framework === 'None') {
+                                .then(framework => {
+                                    if (framework.chosenFramework === 'None') {
+                                        // if no framework, ask him which tech he wants to use
                                         inquirer.prompt(useWebTech)
-                                            .then(answer => {
+                                            .then(tech => {
+                                                // then ask for project params
                                                 inquirer.prompt(projectParams)
-                                                    .then(answers => {
-                                                        createProject(answer.webTech, answers);
+                                                    .then(projectParams => {
+                                                        // create project with all the params
+                                                        createProject(framework.chosenFramework, tech, projectParams);
                                                     });
                                             });
                                     } else {
+                                        // ask for project params
                                         inquirer.prompt(projectParams)
-                                            .then(answers => {
-                                                createProject('none', answers);
+                                            .then(projectParams => {
+                                                // create project with chosen framework and all the params
+                                                createProject(framework.chosenFramework, 'Javascript', projectParams);
                                             });
                                     }
                                 });
