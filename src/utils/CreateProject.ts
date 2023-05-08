fca39672378a5e015728f6420b7e15b2d8b09dcf
Timeuh <timotdu54@gmail.com>
2023-05-08 22:49:09 +0200

    refactor: rework project creation to use react if needed

--- src/utils/CreateProject.ts
+++ src/utils/CreateProject.ts
@@ -3,13 +3,14 @@ import fs from "fs";
 import chalk from "chalk";
 import path from "path";
 import {spawn} from "child_process";
-import {npmPath, npxPath} from "../stores/appConsts";
-import {initEslint} from "./useEslint";
+import {npmPath} from "../stores/appConsts";
+import {initEslint} from "./UseEslint";
+import {initReact} from "./UseReact";
 
 // create a project folder and init all the dependencies
-export const createProject = (answers: CreateProjectAnswers) => {
+export const createProject = (framework: string, webTech: string, answers: CreateProjectAnswers) => {
     // if the user chose to not use a web framework
-    if (answers.webFramework === 'None') {
+    if (framework === 'None') {
         // create new directory
         fs.mkdir(answers.name, (err) => {
             if (err) {
@@ -28,15 +29,17 @@ export const createProject = (answers: CreateProjectAnswers) => {
             });
 
             // init typescript if needed
-            initTypescript(answers.webTech, npmPath);
+            initTypescript(webTech, npmPath);
 
             // init eslint if needed
             if (answers.eslint){
                 initEslint(npmPath);
             }
         });
-    } else {
-        initFramework(answers.name, npxPath);
+    }
+
+    if (framework === 'React') {
+        initReact(answers.name);
     }
 }
 
@@ -50,14 +53,5 @@ const initTypescript = (webTech: string, npmPath: string) => {
     const tsInit = spawn(npmPath, ['install', 'typescript', '--save-dev'], {stdio: 'ignore'});
     tsInit.on('error', () => {
         console.log(chalk.red('Error while initializing Typescript !'));
-    });
-}
-
-// init the chosen web framework
-const initFramework = (projectName: string, npxPath: string) => {
-    // init react project
-    const reactInit = spawn(npxPath, ['create-react-app', projectName], {stdio: 'ignore'});
-    reactInit.on('error', () => {
-        console.log(chalk.red('Error while initializing React !'));
     });
 }
