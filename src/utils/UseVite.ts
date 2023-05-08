84be41ee18ac771e3d5f322d74965fb181dc703d
Timeuh <timotdu54@gmail.com>
2023-05-08 21:41:38 +0200

    refactor: only use vite init and use eslint if needed

--- src/utils/UseVite.ts
+++ src/utils/UseVite.ts
@@ -1,12 +1,27 @@
 import chalk from "chalk";
 import {spawn} from "child_process";
 import {npmPath} from "../stores/appConsts";
+import path from "path";
+import {initEslint} from "./useEslint";
 
 // init Vite js if the user chose it
-export const initVite = () => {
+export const initVite = (useEslint: boolean, projectName: string) => {
     // init vite
-    const viteInit = spawn(npmPath, ['init', 'vite'], {stdio: 'inherit'});
+    const viteInit = spawn(npmPath, ['init', 'vite', projectName], {stdio: 'inherit'});
     viteInit.on('error', () => {
         console.log(chalk.red('Error while initializing Vite !'));
     });
+
+    // is the user wants eslint
+    if (useEslint){
+        // when vite is initialized
+        viteInit.on('close', () => {
+            // change directory to the new one
+            const directoryDir = path.resolve(process.cwd(), projectName);
+            process.chdir(directoryDir);
+
+            // init eslint
+            initEslint(npmPath);
+        });
+    }
 }
