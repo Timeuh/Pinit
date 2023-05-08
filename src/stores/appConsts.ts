8f8f9036873e4ec521fd3dc9aeb8ab33d493a46a
Timeuh <timotdu54@gmail.com>
2023-05-08 20:58:37 +0200

    delete: remove angular and view from framework possibilities (too hard because of their cli)

--- src/stores/appConsts.ts
+++ src/stores/appConsts.ts
@@ -1,5 +1,5 @@
 //choices for web tech of project, between Javascript and Typescript
 export const webTech = ['Javascript', 'Typescript'];
 
-//choices for web framework of project, between React, Vue, Angular and None
-export const webFramework = ['React', 'Vue', 'Angular', 'None'];
+//choices for web framework of project, between React, Vue and None
+export const webFramework = ['React', 'None'];
