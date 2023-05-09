67e666543044a98e52ddce43ac1a1b506633734f
Timeuh <timotdu54@gmail.com>
2023-05-09 22:46:50 +0200

    feat: pass the use of eslint as a parameter of initReact

--- src/utils/UseReact.ts
+++ src/utils/UseReact.ts
@@ -1,12 +1,26 @@
 // init the chosen web framework
 import chalk from "chalk";
 import {spawn} from "child_process";
-import {npxPath} from "../stores/appConsts";
+import {npmPath, npxPath} from "../stores/appConsts";
+import {initEslint} from "./UseEslint";
+import path from "path";
 
-export const initReact = (projectName: string) => {
+export const initReact = (projectName: string, useEslint: boolean) => {
     // init react project
     const reactInit = spawn(npxPath, ['create-react-app', projectName], {stdio: 'ignore'});
     reactInit.on('error', () => {
         console.log(chalk.red('Error while initializing React !'));
+    });
+
+    reactInit.on('close', () => {
+        // init eslint if needed
+        if (useEslint){
+            // change directory to the new one
+            const directoryDir = path.resolve(process.cwd(), projectName);
+            process.chdir(directoryDir);
+
+            // init eslint
+            initEslint(npmPath);
+        }
     });
 }
